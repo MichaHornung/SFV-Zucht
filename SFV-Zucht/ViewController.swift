80882e8deca06d431164c5cf14d274a1cf8ba108
MichaHornung <michaelhornung2022@icloud.com>
2022-12-17 13:37:44 +0100

    Initial Commit

---
+++ SFV-Zucht/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SFV-Zucht
+//
+//  Created by Michael Hornung on 17.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
