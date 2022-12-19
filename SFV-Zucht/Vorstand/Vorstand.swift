18cb4e1ea1c09a52a17a085404b45c65cbc8af43
MichaHornung <michaelhornung2022@icloud.com>
2022-12-19 18:59:58 +0100

    vorstand hinzugefügt weitere Details bearbeitet

--- SFV-Zucht/Vorstand/Vorstand.swift
+++ SFV-Zucht/Vorstand/Vorstand.swift
@@ -11,11 +11,27 @@ struct Vorstand {
     var pic: UIImage
     var titel: String
     var name: String
-    var emailadresse: String
-    var telefonnummer: String
+
 }
 
-var rolandHeilmann = Vorstand(pic: UIImage(named: "RolandHeilmann")!, titel: "1. Vorsitzender", name: "Roland Heilmann", emailadresse: "noch nicht bekannt", telefonnummer: "0176/20829946")
+var rolandHeilmann = Vorstand(pic: UIImage(named: "RolandHeilmann")!, titel: "1. Vorsitzender", name: "Roland Heilmann")
+var thomasWuest = Vorstand(pic: UIImage(named: "ThomasWuest")!, titel: "2. Vorsitzender", name: "Thomas Wüst")
+var sandraNeuhart = Vorstand(pic: UIImage(named: "noPic")!, titel: "Kassenwart", name: "Sandra Neuhart")
+var ronnieForstner = Vorstand(pic: UIImage(named: "noPic")!, titel: "1. Schriftführer", name: "Ronnie Forstner")
+var christophPiecha = Vorstand(pic: UIImage(named: "christophPiecha")!, titel: "2. Schriftführer", name: "Christoph Piecha")
+var stefanScheurer = Vorstand(pic: UIImage(named: "stefanScheurer")!, titel: "1. Gewässerwart", name: "Stefan Scheurer")
+var svenButzinger = Vorstand(pic: UIImage(named: "svenButzinger")!, titel: "2. Gewässerwart", name: "Sven Butzinger")
+var dennisLeonhart = Vorstand(pic: UIImage(named: "dennisLeonhart")!, titel: "3. Gewässerwart", name: "Dennis Leonhart")
+var norbertKitt = Vorstand(pic: UIImage(named: "norbertKitt")!, titel: "Jugendwart", name: "Norbert Kitt")
+var silvesterPfirrmann = Vorstand(pic: UIImage(named: "silvesterPfirrmann")!, titel: "Beisitzer", name: "Silvester Pfirrmann")
+var manuelKloedy = Vorstand(pic: UIImage(named: "manuelKloedy")!, titel: "Beisitzer", name: "Manuel Klödy")
+var christianFritz = Vorstand(pic: UIImage(named: "noPic")!, titel: "Beisitzer", name: "Christian Fritz")
+var matthiasSahm = Vorstand(pic: UIImage(named: "matthiasSahm")!, titel: "Beisitzer", name: "Matthias Sahm")
+var maikHandorf = Vorstand(pic: UIImage(named: "maikHandorf")!, titel: "Beisitzer", name: "Maik Handorf")
+var stefanNeuhart = Vorstand(pic: UIImage(named: "noPic")!, titel: "Vorstandhelfer", name: "Stefan Neuhart")
+var kaiSahm = Vorstand(pic: UIImage(named: "noPic")!, titel: "Vorstandhelfer", name: "Kai Sahm")
+var manfredSchmitz = Vorstand(pic: UIImage(named: "manfredSchmitz")!, titel: "Ehrenvorsitzender", name: "Manfred Schmitz")
 
 
-var vorstand_array: Array = [rolandHeilmann]
+
+var vorstand_array: Array = [rolandHeilmann,thomasWuest,sandraNeuhart,ronnieForstner,christophPiecha,stefanScheurer,svenButzinger,dennisLeonhart,norbertKitt,silvesterPfirrmann,manuelKloedy,christianFritz,matthiasSahm,maikHandorf,stefanNeuhart,kaiSahm,manfredSchmitz]
--- SFV-Zucht/Vorstand/VorstandSFVViewController.swift
+++ SFV-Zucht/Vorstand/VorstandSFVViewController.swift
@@ -1,5 +1,5 @@
 //
-//  VorstandsTableViewController.swift
+//  VorstandSFVViewController.swift
 //  SFV-Zucht
 //
 //  Created by Michael Hornung on 18.12.22.
@@ -7,22 +7,21 @@
 
 import UIKit
 
-class VorstandTableViewController: UIViewController, UITableViewDataSource {
+class VorstandSFVTableViewController: UIViewController, UITableViewDataSource {
     
     
 
-    @IBOutlet weak var name: UITableView!
-    @IBOutlet weak var titel: UITableView!
-    @IBOutlet weak var emailadresse: UITableView!
-    @IBOutlet weak var telefonnummer: UITableView!
+    @IBOutlet weak var vorstand: UITableView!
     
-    var vorstand: Vorstand!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         vorstand.dataSource = self
+        
+        
        
     }
+
         
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return vorstand_array.count
@@ -35,11 +34,11 @@ class VorstandTableViewController: UIViewController, UITableViewDataSource {
         var vorstandmitglied = cell.defaultContentConfiguration()
         
         vorstandmitglied.text = vorstand_array[indexPath.row].titel
-        vorstandmitglied.text = vorstand_array[indexPath.row].name
-        vorstandmitglied.text = vorstand_array[indexPath.row].emailadresse
-        vorstandmitglied.text = vorstand_array[indexPath.row].telefonnummer
+        vorstandmitglied.secondaryText = vorstand_array[indexPath.row].name
+        vorstandmitglied.image = vorstand_array[indexPath.row].pic
         vorstandmitglied.imageProperties.maximumSize = CGSize(width: 2800,height: 157.5)
-        vorstandmitglied.pic = vorstand_array[indexPath.row].pic
+        cell.contentConfiguration = vorstandmitglied
+        
         
         return cell
     }
