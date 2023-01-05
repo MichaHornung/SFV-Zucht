4e397a28f2d1493e91c7c9033f30800c702801ec
MichaHornung <michaelhornung2022@icloud.com>
2023-01-05 15:52:29 +0100

    Fangliste in Bearbeitung

---
+++ SFV-Zucht/Fangliste/Fangliste.swift
@@ -0,0 +1,8 @@
+//
+//  Fangliste.swift
+//  SFV-Zucht
+//
+//  Created by Michael Hornung on 04.01.23.
+//
+
+import Foundation
---
+++ SFV-Zucht/Mitgliederbereich/Mitgliederbereich.swift
@@ -0,0 +1,8 @@
+//
+//  Mitgliederbereich.swift
+//  SFV-Zucht
+//
+//  Created by Michael Hornung on 04.01.23.
+//
+
+import Foundation
