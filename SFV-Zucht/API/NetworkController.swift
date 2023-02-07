90d521a6ffa247057f3a648388df22032058d188
MichaHornung <michaelhornung2022@icloud.com>
2023-02-07 09:49:20 +0100

    Login bearbeitet

--- SFV-Zucht/API/NetworkController.swift
+++ SFV-Zucht/API/NetworkController.swift
@@ -26,7 +26,7 @@ struct NetworkController {
         let session = URLSession.shared
         let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
             if (error != nil) {
-                print(error)
+                print(error!)
             } else {
                 let httpResponse = response as? HTTPURLResponse
                 
